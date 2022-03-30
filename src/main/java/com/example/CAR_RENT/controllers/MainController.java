8a23b6e99a916bd0505acdf2f9277491ffe7f526
Degtyarev02 <vladmirdegty@gmail.com>
2022-03-30 11:07:46 +0300

    javadoc

--- src/main/java/com/example/CAR_RENT/controllers/MainController.java
+++ src/main/java/com/example/CAR_RENT/controllers/MainController.java
@@ -39,5 +39,4 @@ public class MainController {
         model.addAttribute("currentUser", user);
         return "carview";
     }
-
 }
--- src/main/java/com/example/CAR_RENT/controllers/ReviewController.java
+++ src/main/java/com/example/CAR_RENT/controllers/ReviewController.java
@@ -5,10 +5,10 @@ import com.example.CAR_RENT.entity.Review;
 import com.example.CAR_RENT.entity.User;
 import com.example.CAR_RENT.service.repos.CarRepo;
 import com.example.CAR_RENT.service.repos.ReviewsRepo;
-import com.example.CAR_RENT.service.repos.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -28,15 +28,21 @@ public class ReviewController {
      *
      * @param car         автомобиль, для которого пишется отзыв
      * @param currentUser автор отзыва
-     * @param review      текст отзыва
+     * @param review      отзыв, который валидируется согласно модели
      * @return редирект на страницу машины
      */
     @PostMapping("/review/{car}")
-    public String sendReview(@PathVariable Car car, @AuthenticationPrincipal User currentUser, @Valid Review review) {
-        review.setAuthor(currentUser);
-        reviewsRepo.save(review);
-        car.getReviews().add(review);
-        carRepo.save(car);
+    public String sendReview(@PathVariable Car car,
+                             @AuthenticationPrincipal User currentUser,
+                             @Valid Review review,
+                             BindingResult bindingResult) {
+
+        if (!bindingResult.hasErrors()) {
+            review.setAuthor(currentUser);
+            reviewsRepo.save(review);
+            car.getReviews().add(review);
+            carRepo.save(car);
+        }
         return "redirect:/car/" + car.getId();
     }
 }
